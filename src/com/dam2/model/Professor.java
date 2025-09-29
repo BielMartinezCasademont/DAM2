b5df9ebe48978ed09040a58201ba890e6eecfcfa
alaric <alaricros@inspladelestany.cat>
2025-09-29 19:46:26 +0200

    Canvis professor db connection

--- src/com/dam2/model/Professor.java
+++ src/com/dam2/model/Professor.java
@@ -1,4 +1,32 @@
 package com.dam2.model;
 
 public class Professor {
+    private int id;
+    private String nom;
+    private String cognoms;
+
+    public Professor() {}
+
+    public Professor(String nom, String cognoms) {
+        this.nom = nom;
+        this.cognoms = cognoms;
+    }
+
+    public Professor(int id, String nom, String cognoms) {
+        this.id = id;
+        this.nom = nom;
+        this.cognoms = cognoms;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getCognoms() { return cognoms; }
+    public void setCognoms(String cognoms) { this.cognoms = cognoms; }
+
+    @Override
+    public String toString() {
+        return id + " - " + nom + " " + cognoms;
+    }
 }
--- src/com/dam2/util/DBConnection.java
+++ src/com/dam2/util/DBConnection.java
@@ -1,4 +1,15 @@
 package com.dam2.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/DAM2?serverTimezone=UTC&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASS = "12345678";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
